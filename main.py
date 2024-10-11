0ae23e362fced0979ef38ff4e56230e9eafa83a6
Mathis <98806470+MathisFngl@users.noreply.github.com>
2024-10-11 17:21:52 +0200

    main update

--- main.py
+++ main.py
@@ -2,15 +2,13 @@ tableau_de_jeu = [[0 for i in range(5)] for j in range(5)]
 for li in tableau_de_jeu:
     print(li)
 
-
 class Pion:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-    def build(X,Y):
+    def build(X ,Y):
         if tableau_de_jeu[X][Y] < 4:
             tableau_de_jeu[X][Y] += 1
-
 
 class Joueur:
     def __init__(self, name, dieu):
@@ -19,5 +17,3 @@ class Joueur:
         self.pion2 = Pion.__init__(self, 0, 1)
 
         self.pouvoir_de_dieu = dieu
-
-
