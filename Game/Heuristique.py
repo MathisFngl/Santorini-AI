0870d42d43e424870a49f7cae5358cd74c74c672
AlexiCor <correard.alexis@gmail.com>
2024-12-09 22:11:01 +0100

    Ajout de fonctions Helper heuristique

--- Game/Heuristique.py
+++ Game/Heuristique.py
@@ -54,6 +54,17 @@ def isCentralPosition(x, y):
     return 1 <= x <= 3 and 1 <= y <= 3
 
 
+def distanceToCentralPosition(pawn):
+    """
+    Calculate the straight-line distance between the center of the board and a pawn.
+    :param pawn: The pawn with getCoordinates() method.
+    :return: The straight-line distance (hypotenuse) between the two points.
+    """
+    x1, y1 = pawn.getCoordinates()
+    x2, y2 = 2, 2
+    distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+    return distance
+
 def euclidean_distance(pawn1, pawn2):
     """
     Calculate the straight-line distance between two pawns.
@@ -114,3 +125,54 @@ def isPawnBlocked(p, tableau, pawns):
                     continue  # The cell is blocked, cycle the loop
                 return False  # If at least one of the 8 surrounding cells is free, the pawn is NOT blocked
     return True
+
+def countCompletedTowers(tableau):
+    """
+    Count the total number of completed towers (level 4) on the board.
+    :param tableau: The 5x5 game board containing tower values.
+    :return: Number of completed towers on the board
+    """
+    total_count = 0
+    for row in tableau:
+        for cell in row:
+            if cell == 4:
+                total_count += 1
+    return total_count
+
+def winningPawn(p, tableau, pawns):
+    """
+    Check if a pawn can make a winning move by moving to a level 3 tower.
+    :param p: The current pawn (object with coordinates x, y).
+    :param tableau: The 5x5 game board containing tower values.
+    :param pawns: List of pawns (objects with x, y) representing other pawns.
+    :return: True if the pawn can win by moving, False otherwise.
+    """
+    x, y = p.getCoordinates()
+    if tableau[x][y] != 2:
+        return False
+
+    for dx in range(-1, 2):
+        for dy in range(-1, 2):
+            if dx == 0 and dy == 0:
+                continue
+
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < 5 and 0 <= ny < 5:
+                if any(pawn.x == nx and pawn.y == ny for pawn in pawns):
+                    continue
+                if tableau[nx][ny] == 3:
+                    return True
+    return False
+
+def countTotalConstructions(tableau):
+    """
+    Calculate the total number of constructions on the board.
+    :param tableau: The 5x5 game board containing tower values.
+    :return: The total number of constructions
+    """
+    total_constructions = 0
+
+    for row in tableau:
+        for cell in row:
+            total_constructions += cell
+    return total_constructions
